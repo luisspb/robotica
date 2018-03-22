07c1474fdb5e2c8e7cd8c12ad051cd48fedf88d8
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-03-22 16:34:58 -0300

    - Adiciona implementacao da lista de vizinhos no A estrela.

--- matlab/a_star.m
+++ matlab/a_star.m
@@ -4,23 +4,18 @@
 
 % Algoritmo A*
 
-%% No inicial
-% start_node.x = 1;
-% start_node.y = 1;
-% start_node.g = ;
-% start_node.f = ;
-
-%% No final
-% goal_node.x = 9;
-% goal_node.y = 9;
-% goal_node.g = ;
-% goal_node.f = 0;
+%% Estrutura do No
+% node.x = 1;
+% node.y = 1;
+% node.g = 1;
+% node.f = 8;
+% node.parent = 0;
 
 function nodes_path = a_star(start_node, goal_node)
 
    closed_set = [];
    open_set = [start_node];
-   came_from = [];
+   path = [];
 
    start_node.g = 0;
    start_node.f = start_node.g + manhattan_distance(start_node, goal_node);
@@ -43,10 +38,29 @@ function nodes_path = a_star(start_node, goal_node)
       % Adiciona current_node em closed_set
       closed_set = [closed_set current_node];
 
-      % Cria lista de vizinhos
-      node = current_node;
-      node = current_node.x
-      neighbor_nodes = 
+      %% Lista de vizinhos
+      % Vizinho de cima
+      node_up = current_node;
+      node_up.y = node_up.y + 1;
+      % Vizinho de baixo
+      node_down = current_node;
+      node_down.y = node_down.y - 1;
+      % Vizinho da esquerda
+      node_left = current_node;
+      node_left.x = node_left.x - 1;
+      % Vizinho da direita
+      node_right = current_node;
+      node_right.x = node_right.x + 1;
+      % Cria a lista de vizinhos
+      neighbor_nodes = [node_up node_down node_left node_right];
+
+      for neighbor = neighbor_nodes
+
+         
+
+      end
+
+
 
    end
 
