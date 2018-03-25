19a1970c8e486cd556af3d9ec0b1a4a33300aa6f
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-03-25 17:08:25 -0300

    - Completa script de teste.

--- matlab/test_a_star.m
+++ matlab/test_a_star.m
@@ -17,3 +17,17 @@ INF = 100000;
 GRID_MIN = 1;
 GRID_MAX = 5;
 
+grid = [1 1 1 1 1;
+        1 1 1 1 1;
+        1 1 1 1 1;
+        1 1 1 1 1;
+        1 1 1 1 1];
+
+start_node.x = 1;
+start_node.y = 1;
+
+goal_node.x = 4;
+goal_node.y = 3;
+
+path_nodes = a_star(start_node, goal_node);
+
