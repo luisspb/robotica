9907d72661c4af79929a34c9fae148bb0a3ca549
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-03-25 16:11:16 -0300

    - Corrige e melhora funcao de criacao da lista de vizinhos:  * Estava incompleta;  * Nao levava em consideracao os limites do grid de celulas.

--- matlab/neighbor_list.m
+++ matlab/neighbor_list.m
@@ -7,39 +7,51 @@
 
 function neighbor_nodes = neighbor_list(current_node)
 
-      % Vizinho de cima
-      node_up = current_node;
-      node_up.y = node_up.y + 1;
-      % is_blocked? Adicionar depois!
+   neighbor_nodes = [];
+
+   % Vizinho de cima
+   node_up = current_node;
+   node_up.y = node_up.y + 1;
+   % is_blocked? Adicionar depois!
+   if node_up.y <= GRID_MAX
       node_up.g = INF;
       node_up.f = INF;
-      node_up.parent = current_node;
+      node_up.camefrom = current_node;
+      neighbor_nodes = [neighbor_nodes node_up];
+   end
 
-      % Vizinho de baixo
-      node_down = current_node;
-      node_down.y = node_down.y - 1;
-      % is_blocked? Adicionar depois!
-      node_up.g = INF;
-      node_up.f = INF;
-      node_up.parent = current_node;
+   % Vizinho de baixo
+   node_down = current_node;
+   node_down.y = node_down.y - 1;
+   % is_blocked? Adicionar depois!
+   if node_down.y >= GRID_MIN
+      node_down.g = INF;
+      node_down.f = INF;
+      node_down.camefrom = current_node;
+      neighbor_nodes = [neighbor_nodes node_down];
+   end
 
-      % Vizinho da esquerda
-      node_left = current_node;
-      node_left.x = node_left.x - 1;
-      % is_blocked? Adicionar depois!
-      node_up.g = INF;
-      node_up.f = INF;
-      node_up.parent = current_node;
+   % Vizinho da esquerda
+   node_left = current_node;
+   node_left.x = node_left.x - 1;
+   % is_blocked? Adicionar depois!
+   if node_left.x >= GRID_MIN
+      node_left.g = INF;
+      node_left.f = INF;
+      node_left.camefrom = current_node;
+      neighbor_nodes = [neighbor_nodes node_left];
+   end
 
-      % Vizinho da direita
-      node_right = current_node;
-      node_right.x = node_right.x + 1;
-      % is_blocked? Adicionar depois!
-      node_up.g = INF;
-      node_up.f = INF;
-      node_up.parent = current_node;
+   % Vizinho da direita
+   node_right = current_node;
+   node_right.x = node_right.x + 1;
+   % is_blocked? Adicionar depois!
+   if node_right.x <= GRID_MAX
+      node_right.g = INF;
+      node_right.f = INF;
+      node_right.camefrom = current_node;
+      neighbor_nodes = [neighbor_nodes node_right];
+   end
 
-      % Cria a lista de vizinhos
-      neighbor_nodes = [node_up node_down node_left node_right];
 end
 
