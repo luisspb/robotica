ed948ef10a6106c8860a72bbd26a5d51cc21abc0
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-04-30 14:46:08 -0300

    Adiciona ultimas modificacoes antes da primeira entrega.

--- matlab/test_a_star.m
+++ matlab/test_a_star.m
@@ -11,13 +11,15 @@
 % node.f = INF (valor default = infinito)
 % node.camefrom = NULL
 
+clear
+
 % Valor 'infinito'
 INF = 100000;
 % Limites do grid de celulas
 GRID_MIN = 1;
 GRID_MAX = 5;
 
-grid = [1 1 1 1 1;
+grid_ = [1 1 1 1 1;
         1 1 1 1 1;
         1 1 1 1 1;
         1 1 1 1 1;
@@ -29,11 +31,32 @@ start_node.g = INF;
 start_node.f = INF;
 start_node.camefrom = [];
 
-goal_node.x = 4;
-goal_node.y = 3;
+goal_node.x = 3;
+goal_node.y = 4;
 goal_node.g = INF;
 goal_node.f = INF;
 goal_node.camefrom = [];
 
 path_nodes = a_star(start_node, goal_node);
+
+x = [];
+y = [];
+
+for i = path_nodes
+   x = [x i.x];
+   y = [y i.y];
+end
+
+x(1) = [];
+x(end) = [];
+
+y(1) = [];
+y(end) = [];
+
+axis([GRID_MIN-1 GRID_MAX+1 GRID_MIN-1 GRID_MAX+1])
+grid on
+hold on
+stem(start_node.x, start_node.y, 'filled', 'LineStyle', 'none', 'MarkerFaceColor','green')
+stem(goal_node.x, goal_node.y, 'filled', 'LineStyle', 'none', 'MarkerFaceColor','red')
+stem(x, y, 'filled', 'LineStyle', 'none', 'MarkerFaceColor','blue')
 
