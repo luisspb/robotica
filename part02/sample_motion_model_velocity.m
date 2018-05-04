7eb7ef2eddbcefc2c375ecef2505a6ff23d41ab9
saulobezerra <sauloalves89@gmail.com>
2018-05-04 15:10:45 -0300

    Update sample_motion_model_velocity.m

--- part02/sample_motion_model_velocity.m
+++ part02/sample_motion_model_velocity.m
@@ -6,21 +6,21 @@ function position = sample_motion_model_velocity(Ut,Xt1)
 	y = Xt1(2);
 	teta = Xt1(3);
     
-    %alfa 1 to 6 s„o paramentros de erro de movimento especifico do robo.
+    %alfa 1 to 6 s√£o paramentros de erro de movimento especifico do robo.
     alfa1 = 0.2; alfa2 = 0.2; alfa3 = 0.2; alfa4 = 0.2; alfa5 = 0.2; alfa6 = 0.2;
-    %DeltaT duraÁ„o fixa de tempo.
+    %DeltaT dura√ß√£o fixa de tempo.
     DeltaT = 3.0;
-	%N„o tenho absoluta cereza desses valores acima
+	%N√£o tenho absoluta cereza desses valores acima
 	
 	vc = v + sample(alfa1*abs(v) + alfa2*abs(w));
 	wc = w + sample(alfa3*abs(v) + alfa4*abs(w));
 	gamac = sample(alfa5*abs(v) + alfa6*abs(w));
+	
 	xl = x - (vc/wc)*sin(teta) + (vc/wc)*sin(teta + wc*DeltaT);
-	yl = y + (vc/wc)*cos(teta) - (vc/wc)*cos(teta + wc*DeltaT);
-    
+	yl = y + (vc/wc)*cos(teta) - (vc/wc)*cos(teta + wc*DeltaT);    
 	tetaL = teta + wc*DeltaT + gamac*DeltaT;
 	
-	xt = [xl,yl,tetaL];	% acho que √© transposta (matriz coluna)
+	xt = [xl,yl,tetaL];	
 	position = xt;
 	
 end
