a2454beb5c5535973b92d3dfd72a8ec9fa05a0eb
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-06-12 15:18:11 -0300

    Atualiza modelo do gazebo.

--- gazebo_model/gazebo_turtlebot_laser.sh
+++ gazebo_model/gazebo_turtlebot_laser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/indigo/setup.bash
 export ROS_IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
-roslaunch turtlebot_gazebo turtlebot_laser.launch
+roslaunch turtlebot_gazebo turtlebot_laser2.launch
