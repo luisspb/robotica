eff58343d405d0bd26582cb4fd35621f90d0091a
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-06-12 17:20:14 -0300

    Seta posicao do robo no script que chama o gazebo.

--- gazebo_model/gazebo_turtlebot_laser.sh
+++ gazebo_model/gazebo_turtlebot_laser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/indigo/setup.bash
 export ROS_IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
-roslaunch turtlebot_gazebo turtlebot_laser2.launch
+ROBOT_INITIAL_POSE="-x 2 -y 2" roslaunch turtlebot_gazebo turtlebot_laser2.launch
